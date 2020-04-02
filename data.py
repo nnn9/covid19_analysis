6fbd713e7a0a6f1bd64f8676a30ac3ac2fe4488e
Nathan N <nnastili@gmail.com>
2020-04-01 22:03:53 -0400

    scrapped country population - df needs to be cleaned

--- data.py
+++ data.py
@@ -1,5 +1,32 @@
+from bs4 import BeautifulSoup  # Include BS web scraping module
+import requests
 import git
 import pandas as pd
+
+## Pull Country Data
+
+url = "https://www.worldometers.info/world-population/population-by-country/"
+res = requests.get(url, headers={"User-Agent":"Mozilla/5.0"})
+print(res.status_code)
+soup = BeautifulSoup(res.text, "html.parser") # Parses HTTP Response
+
+table_head = soup.findChildren('thead')[0]
+column_header = [i.get_text() for i in table_head.findAll('th')]
+column_header
+
+table_data = soup.findChildren('tbody')[0]
+rows = table_data.findChildren(['th', 'tr'])
+
+country_data = []
+for row in rows:
+    cells = row.findChildren('td')
+    country_data.append([i.string for i in cells])
+
+country_data_df = pd.DataFrame(country_data, columns=column_header)
+## Need to clean data frame
+
+
+## Pull Covid Data
 
 #git.Git("./public_data").clone("https://github.com/CSSEGISandData/COVID-19.git")
 #git.Git("./public_data").clone("https://github.com/nytimes/covid-19-data.git")
@@ -11,3 +38,4 @@ git.cmd.Git("./public_data/covid-19-data").pull()
 global_confirmed_ts = pd.read_csv("public_data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 global_recovered_ts = pd.read_csv("public_data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
 global_deaths_ts = pd.read_csv("public_data/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+
